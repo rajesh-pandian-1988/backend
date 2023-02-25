34a795ba8e9fa20c280c1e38a15613fc43ca9aa4
Rajesh Pandian <rajesh_pandian@outlook.com>
2023-02-25 08:50:10 +0000

    updating keys

--- providers.tf
+++ providers.tf
@@ -9,7 +9,7 @@ terraform {
     resource_group_name  = "tfstate"
     storage_account_name = "tfstate0637"
     container_name       = "tfstate"
-    key                  = "terraform.tfstate"
+    key                  = "sGLOhNoHjkckpygo/AE1UrOCt8Q67H8LEx9FWO/fOW2QKSfJgLyypEjQESs03z44eUP9Oo7yEmou+AStMrc0nw=="
   }
 }
 
